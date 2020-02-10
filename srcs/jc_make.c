2b3a364111d511e301a1da8a656470ad8b3cf0f5
skamo3 <joochan123123@gmail.com>
2020-02-10 18:50:57 +0900

    aa

--- srcs/jc_make.c
+++ srcs/jc_make.c
@@ -1,16 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   j_boxcheck.c                                       :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junkang <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: joockim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/10 18:13:05 by junkang           #+#    #+#             */
-/*   Updated: 2020/02/10 18:30:47 by junkang          ###   ########.fr       */
+/*   Created: 2020/02/10 18:00:23 by joockim           #+#    #+#             */
+/*   Updated: 2020/02/10 18:50:16 by joockim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_bsq.h"
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
 
 int		box_check(int i, int j, t_map_info info)
 {
@@ -26,4 +31,29 @@ int		box_check(int i, int j, t_map_info info)
 	if (i >= i_min && i <= i_max && j >= j_min && j <= j_max)
 		return (1);
 	return (0);
+}
+
+void	print_map(int **map, t_map_info info)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= max_col)
+	{
+		j = 0;
+		while (j <= max_row)
+		{
+			if (map[i][j] == 0)
+				ft_putchar(info.word[1]);
+			else if (box_check(i, j, info))
+				ft_putchar(info.word[2]);
+			else
+				ft_putchar(info.word[0]);
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
+	}
+	write(1, "\n", 1);
 }
