163b8b57d7570e3f24857c9b4eddd7e10fcab0f1
junkang <nawaddaing@gmail.com>
2020-02-10 21:05:27 +0900

    as

--- srcs/j_main.c
+++ srcs/j_main.c
@@ -6,13 +6,13 @@
 /*   By: junkang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 20:44:25 by junkang           #+#    #+#             */
-/*   Updated: 2020/02/10 20:59:11 by junkang          ###   ########.fr       */
+/*   Updated: 2020/02/10 21:04:59 by junkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_bsq.h"
 
-int main(void)
+int		main(void)
 {
 	t_map_int_info	info;
 	int				i;
--- srcs/y_sol.c
+++ srcs/y_sol.c
@@ -6,7 +6,7 @@
 /*   By: youlee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 17:34:44 by youlee            #+#    #+#             */
-/*   Updated: 2020/02/10 20:59:00 by junkang          ###   ########.fr       */
+/*   Updated: 2020/02/10 21:04:42 by junkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	checkmax(int row, int col, int val, t_map_info *info)
 {
-	if (val > info -> val)
+	if (val > info->val)
 	{
-		info -> row = row;
-		info -> col = col;
-		info -> val = val;
+		info->row = row;
+		info->col = col;
+		info->val = val;
 	}
 }
 
-int		minn(int a,int b)
+int		minn(int a, int b)
 {
 	if (a > b)
-		return b;
+		return (b);
 	else
-		return a;
+		return (a);
 }
 
 void	dynamic(int (*map)[10], t_map_info *info)
@@ -37,14 +37,14 @@ void	dynamic(int (*map)[10], t_map_info *info)
 
 	idx = 1;
 	idx2 = 1;
-	while (idx < info -> max_row)
+	while (idx < info->max_row)
 	{
-		while (idx2 < info -> max_col)
+		while (idx2 < info->max_col)
 		{
 			if (map[idx][idx2] != 0)
 			{
-				map[idx][idx2] = minn(map[idx-1][idx2], map[idx][idx2-1]);
-				map[idx][idx2] = minn(map[idx][idx2], map[idx-1][idx2-1]);
+				map[idx][idx2] = minn(map[idx - 1][idx2], map[idx][idx2 - 1]);
+				map[idx][idx2] = minn(map[idx][idx2], map[idx - 1][idx2 - 1]);
 				map[idx][idx2]++;
 			}
 			checkmax(idx, idx2, map[idx][idx2], info);
