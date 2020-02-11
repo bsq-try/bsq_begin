27ca87d7f009dd1ea3937dcc9066bb0d0c6d4e9f
junkang <nawaddaing@gmail.com>
2020-02-11 21:55:24 +0900

    aaa

--- srcs/j_main.c
+++ srcs/j_main.c
@@ -6,22 +6,23 @@
 /*   By: junkang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 20:44:25 by junkang           #+#    #+#             */
-/*   Updated: 2020/02/11 21:15:45 by junkang          ###   ########.fr       */
+/*   Updated: 2020/02/11 21:54:36 by junkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_bsq.h"
 
-int		main(void)
+int		main(int argc, char *argv[])
 {
-	t_map_int_info	info;
+	t_map_info		info;
 	int				i;
 	int				**map;
-
+	char			*map2;
 	i = 1;
 	while (i < argc)
 	{
-		map = reader_control(argv[i], &info);
+		map2 = one_d_array_f(argv[i], &info);
+		map = char_to_int_map(map2, &info);
 		dynamic(map, &info);
 		print_map(map, info);
 		j_free(map, info);
--- srcs/y_convert.c
+++ srcs/y_convert.c
@@ -6,12 +6,14 @@
 /*   By: youlee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 17:04:50 by youlee            #+#    #+#             */
-/*   Updated: 2020/02/11 21:23:12 by junkang          ###   ########.fr       */
+/*   Updated: 2020/02/11 21:47:22 by junkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //char *map 을 int ** map 으로 바꾸어주는 작업.
 
+#include "../includes/ft_bsq.h"
+#include <stdio.h>
 int	**char_to_int_map(char *cmap, t_map_info *info)
 {
 	int index;
@@ -28,14 +30,20 @@ int	**char_to_int_map(char *cmap, t_map_info *info)
 		imap[index] = (int *)malloc(sizeof(int *) * (info->max_col));
 		while (index2 < info->max_col)
 		{
-			if (cmap[index][index2] == info->word[0])
-				imap[index3] = 1;
-			else if (cmap[index][index2] == info->word[1])
-				imap[index3] = 0;
+			if (cmap[index3] == info->word[0])
+				imap[index][index2] = 1;
+			else if (cmap[index3] == info->word[1])
+				imap[index][index2] = 0;
 			index2++;
 			index3++;
 		}
 		index++;
+	}
+	for(int i=0;i<index;i++)
+	{
+		for(int j=0;j<index2;j++)
+			printf("%d ",imap[i][j]);
+		printf("\n");
 	}
 	free(cmap);
 	return (imap);
