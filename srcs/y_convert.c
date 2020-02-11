53faaf171d2cae95b50679d76e56cdbb38042a8f
Youlee <ukjinlee66@github.com>
2020-02-11 20:24:41 +0900

    youlee

--- srcs/y_convert.c
+++ srcs/y_convert.c
@@ -6,10 +6,10 @@
 /*   By: youlee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 17:04:50 by youlee            #+#    #+#             */
-/*   Updated: 2020/02/11 18:42:06 by youlee           ###   ########.fr       */
+/*   Updated: 2020/02/11 20:24:17 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//char **map 을 int ** map 으로 바꾸어주는 작업.
+//char *map 을 int ** map 으로 바꾸어주는 작업.
 void	free_cmap(char **cmap, t_map_info info)
 {
 	int index;
@@ -20,27 +20,30 @@ void	free_cmap(char **cmap, t_map_info info)
 	free(cmap);
 }
 
-int	**char_to_int_map(char **cmap, t_map_info *info)
+int	**char_to_int_map(char *cmap, t_map_info *info)
 {
-	int idx;
-	int idx2;
+	int index;
+	int index2;
+	int index3;
 	int **imap;
 
+	index3 = 0;
 	imap = (int **)malloc(sizeof(int *) * (info -> max_row));
-	idx = 0;
-	while (idx < info -> max_row)
+	index = 0;
+	while (index < info -> max_row)
 	{
-		idx2 = 0;
-		imap[idx] = (int *)malloc(sizeof(int *) * (info -> max_col));
-		while (idx2 < info -> max_col)
+		index2 = 0;
+		imap[index] = (int *)malloc(sizeof(int *) * (info -> max_col));
+		while (index2 < info -> max_col)
 		{
-			if (cmap[idx][idx2] == info -> word[0])
-				imap[idx][idx2] = 1;
-			else if(cmap[idx][idx2] == info -> word[1])
-				imap[idx][idx2] = 0;
-			idx2++;
+			if (cmap[index][index2] == info -> word[0])
+				imap[index3] = 1;
+			else if(cmap[index][index2] == info -> word[1])
+				imap[index3] = 0;
+			index2++;
+			index3++;
 		}
-		idx++;
+		index++;
 	}
 	free_cmap(cmap, info);
 	return (imap);
