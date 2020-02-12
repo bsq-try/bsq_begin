0f983564d9eec3b298706df3ac0601a8aa28675d
skamo3 <joochan123123@gmail.com>
2020-02-12 19:52:35 +0900

    finish

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = gcc
 CFLAGS =  -Werror -Wall -Wextra
 NAME = BSQ
 INCL = ./includes
-SRCS = $(addprefix ./srcs/, j_first_line.c jc_make.c j_free.c s_function.c y_convert.c j_main.c s_reader.c y_sol.c)
+SRCS = $(addprefix ./srcs/, first_line.c make.c free.c function.c convert.c main.c reader.c sol.c)
 OBJS = $(SRCS:.c=.o)
 
 .PHONY : all clean fclean
--- includes/ft_bsq.h
+++ includes/ft_bsq.h
@@ -6,17 +6,16 @@
 /*   By: sseo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 14:32:21 by sseo              #+#    #+#             */
-/*   Updated: 2020/02/12 15:42:41 by junkang          ###   ########.fr       */
+/*   Updated: 2020/02/12 19:52:24 by joockim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BSQ_H
 # define FT_BSQ_H
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 typedef struct	s_map_info
 {
@@ -25,37 +24,29 @@ typedef struct	s_map_info
 	int			row;
 	int			col;
 	int			val;
-	char		word[3];		/*빈문자 장애물 마킹*/
+	char		word[3];
 }				t_map_info;
 
-/*	j_first_line.c	*/
 int				ft_len(char *str);
 int				char_check(char *str);
 int				ft_atoi(char *b_str, int len);
 int				first_line(char *str, t_map_info *info);
-/*	j_free.c	*/
 void			j_free(int **map, int max_row);
-/*	jc_make.c	*/
 void			ft_putchar(char a);
 int				box_check(int i, int j, t_map_info info);
 void			print_map(int **map, t_map_info info);
-/*	s_function.c	*/
 unsigned int	get_len_to_sep(char *str, char sep, unsigned int start);
 char			*partial(char *str, unsigned int start, unsigned int end);
-/*	s_reader.c	*/
 unsigned int	get_total_len(char *file_name);
 char			*make_one_d_array(char *file_name, unsigned int len);
 int				check_board(char *board, t_map_info *info);
 char			*one_d_array_f(char *file_name, t_map_info *info);
-/*	y_convert.c	*/
 int				mark_one(int **imap, t_map_info *info);
 int				**make_imap(t_map_info *info);
 int				**char_to_int_map(char *cmap, t_map_info *info, int i, int j);
-/*	y_sol.c	*/
 void			checkmax(int row, int col, int val, t_map_info *info);
 int				minn(int a, int b);
 void			dynamic(int **map, t_map_info *info);
-/*	j_main.c	*/
 char			*newline_to_null(char *str);
 int				is_argv(char *file_name);
 
